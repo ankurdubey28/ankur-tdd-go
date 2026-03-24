67611fde2de39e085d1f839f3004d08ffa0dd8c5
ankur <ankurdwivedi.dce@gmail.com>
2026-03-24 22:25:07 +0530

    add example test in testing_example directory

--- testing_example/main.go
+++ testing_example/main.go
@@ -11,14 +11,14 @@ func NotifyUser(sender EmailSender, user string) error {
 // Dummy
 type DummyEmailSender struct{}
 
-func (d DummyEmailSender) Send(to, msg string) error {
+func (d *DummyEmailSender) Send(to, msg string) error {
 	return nil // never actually used
 }
 
 // Stub
 type StubEmailSender struct{}
 
-func (s StubEmailSender) Send(to, msg string) error {
+func (s *StubEmailSender) Send(to, msg string) error {
 	return nil // can be anythin fixed thing depending on return type
 }
 
@@ -29,7 +29,7 @@ type MockEmailSender struct {
 	Msg    string
 }
 
-func (m MockEmailSender) Send(to, msg string) error {
+func (m *MockEmailSender) Send(to, msg string) error {
 	m.Called = true // specific set of steps being followed in a order
 	m.To = to
 	m.Msg = msg
@@ -41,7 +41,7 @@ type FakeEmailSender struct {
 	sent []string
 }
 
-func (f FakeEmailSender) Send(to, msg string) error {
+func (f *FakeEmailSender) Send(to, msg string) error {
 	f.sent = append(f.sent, msg) // simplified implementation of in memory email backup
 	return nil
 }
@@ -52,7 +52,7 @@ type SpiesEmailSender struct {
 	Called bool
 }
 
-func (s SpiesEmailSender) Send(to, msg string) error {
+func (s *SpiesEmailSender) Send(to, msg string) error {
 	s.Called = true // observe flow
 	return s.Real.Send(to, msg)
 }
--- testing_example/main_test.go
+++ testing_example/main_test.go
@@ -1,1 +1,86 @@
 package main
+
+import (
+	"errors"
+	"testing"
+)
+
+// Dummy Test
+func TestNotifyUser_WithDummy(t *testing.T) {
+	sender := DummyEmailSender{}
+
+	err := NotifyUser(&sender, "test@example.com")
+
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+}
+
+// Stub Test
+type ErrorStubEmailSender struct{}
+
+func (s ErrorStubEmailSender) Send(to, msg string) error {
+	return errors.New("stub error")
+}
+
+func TestNotifyUser_WithStub(t *testing.T) {
+	sender := ErrorStubEmailSender{}
+
+	err := NotifyUser(sender, "test@example.com")
+
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+}
+
+// Mock Test
+func TestNotifyUser_WithMock(t *testing.T) {
+	mock := &MockEmailSender{}
+
+	_ = NotifyUser(mock, "test@example.com")
+
+	if !mock.Called {
+		t.Errorf("expected Send to be called")
+	}
+
+	if mock.To != "test@example.com" {
+		t.Errorf("expected recipient to be test@example.com, got %s", mock.To)
+	}
+
+	if mock.Msg != "Welcome !" {
+		t.Errorf("unexpected message: %s", mock.Msg)
+	}
+}
+
+// Fake Test
+func TestNotifyUser_WithFake(t *testing.T) {
+	fake := &FakeEmailSender{}
+
+	_ = NotifyUser(fake, "test@example.com")
+
+	if len(fake.sent) != 1 {
+		t.Errorf("expected 1 email sent, got %d", len(fake.sent))
+	}
+
+	if fake.sent[0] != "Welcome !" {
+		t.Errorf("unexpected message: %s", fake.sent[0])
+	}
+}
+
+// Spy Test
+func TestNotifyUser_WithSpy(t *testing.T) {
+	fake := &FakeEmailSender{}
+	spy := &SpiesEmailSender{
+		Real: fake,
+	}
+
+	_ = NotifyUser(spy, "test@example.com")
+
+	if !spy.Called {
+		t.Errorf("expected spy to record call")
+	}
+
+	if len(fake.sent) != 1 {
+		t.Errorf("expected underlying sender to be called")
+	}
+}
